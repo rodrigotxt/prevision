4bcce49ff06c2e3e4b30b7af6d9ea7f78a45dd47
Rodrigo Martins <rodrigotxt@gmail.com>
2019-10-16 17:22:07 -0300

    Ignore arquivos do desafio: pdf, csv e ajustes no layout index

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,117 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+  <v-app id="inspire">
+    <v-navigation-drawer
+    v-model="drawer"
+    app
+    >
+    <v-list dense>
+      <v-list-item>
+        <v-list-item-action>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-action>
+          <v-icon>mdi-alert-circle</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Sobre</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <v-app-bar
+  app
+  color="indigo"
+  dark
+  >
+  <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-toolbar-title>IonPrev</v-toolbar-title>
+</v-app-bar>
+
+<v-content>
+  <v-container
+  class="fill-height"
+  fluid
+  >
+  <v-row
+  align="center"
+  justify="center"
+  >
+  <v-col class="text-center col-upload" :class="{'drag-active': ($refs.upload && $refs.upload.dropActive)}">
+    <file-upload
+    :post-action="url_api + '/consulta/upload'"
+    class="w-100 py-5"
+    ref="upload"
+    v-model="files"
+    :drop="true"
+    @input-file="inputFile"
+    @input-filter="inputFilter"
+    accept="text/csv"
+    >
+    <h3 class="mb-3">Clique no botão ou arraste aqui seu arquivo .csv</h3>
+    <v-btn depressed large color="primary"><v-icon>mdi-arrow-up-bold</v-icon> Enviar arquivo</v-btn>
+  </file-upload>
+</v-col>
+</v-row>
+</v-container>
+</v-content>
+<v-footer
+color="indigo"
+app
+>
+<span class="white--text">&copy; 2019 - Rodrigo Martins <a class="indigo--text text--lighten-3 " href="https://github.com/rodrigotxt/">@rodrigotxt</a></span>
+</v-footer>
+</v-app>
 </template>
 
+<style scoped>
+.col-upload{
+  transition: 1s all;
+}
+.drag-active {
+  background: linear-gradient(to left, white,lightgreen,white);
+  padding: 10vh 0;
+}
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
   data: () => ({
-    //
+    url_api: '../backend',
+    drawer: true,
+    files: [],
   }),
+  methods: {
+    inputFile: function (newFile, oldFile) {
+      // Automatic upload
+      if (Boolean(newFile) !== Boolean(oldFile) || oldFile.error !== newFile.error) {
+        if (!this.$refs.upload.active) {
+          this.$refs.upload.active = true
+        }
+      }
+    },
+    inputFilter: function (newFile, oldFile, prevent) {
+      if (newFile && !oldFile) {
+        // console.log(newFile,oldFile);
+        if (!/\.(csv)$/i.test(newFile.name)) {
+          alert('Tipo de arquivo não permitido.')
+          return prevent()
+        }
+      }
+    }
+
+  }
 };
 </script>
