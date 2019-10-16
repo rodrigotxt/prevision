15c01933f2c579c4a49dd4c47f806bdd3a452a89
Rodrigo Martins <rodrigotxt@gmail.com>
2019-10-16 15:44:55 -0300

    Adicionado componente para upload

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,10 @@ import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+const VueUploadComponent = require('vue-upload-component')
+Vue.component('file-upload', VueUploadComponent)
+
 new Vue({
-  vuetify,
-  render: h => h(App)
+	vuetify,
+	render: h => h(App)
 }).$mount('#app')
